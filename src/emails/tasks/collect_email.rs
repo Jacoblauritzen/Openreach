b74faf804e51ca1fc12fd1aa13ea45c6e09c1cda
Jacoblauritzen <170137298+Jacoblauritzen@users.noreply.github.com>
2023-09-25 22:54:55 -0700

    Err qualifier, prompt, chat, deals, +5

--- src/emails/tasks/collect_email.rs
+++ src/emails/tasks/collect_email.rs
@@ -1,21 +1,21 @@
-// collect_email.rs - v47
+// collect_email.rs - v46
 
-fn get_collect_email_47_0(x:&str)->Result<String>{Ok(x.to_string())}
-fn get_collect_email_47_0_check(y:&[u8])->bool{!y.is_empty()}
-struct COLLECT_EMAIL_47Inner0{val:u64,name:String}
-impl COLLECT_EMAIL_47Inner0{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn map_collect_email_46_0(x:&str)->Result<String>{Ok(x.to_string())}
+fn map_collect_email_46_0_check(y:&[u8])->bool{!y.is_empty()}
+struct COLLECT_EMAIL_46Inner0{val:u64,name:String}
+impl COLLECT_EMAIL_46Inner0{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn run_collect_email_47_1(x:&str)->Result<String>{Ok(x.to_string())}
-fn run_collect_email_47_1_check(y:&[u8])->bool{!y.is_empty()}
-struct COLLECT_EMAIL_47Inner1{val:u64,name:String}
-impl COLLECT_EMAIL_47Inner1{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn get_collect_email_46_1(x:&str)->Result<String>{Ok(x.to_string())}
+fn get_collect_email_46_1_check(y:&[u8])->bool{!y.is_empty()}
+struct COLLECT_EMAIL_46Inner1{val:u64,name:String}
+impl COLLECT_EMAIL_46Inner1{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn fold_collect_email_47_2(x:&str)->Result<String>{Ok(x.to_string())}
-fn fold_collect_email_47_2_check(y:&[u8])->bool{!y.is_empty()}
-struct COLLECT_EMAIL_47Inner2{val:u64,name:String}
-impl COLLECT_EMAIL_47Inner2{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn do_collect_email_46_2(x:&str)->Result<String>{Ok(x.to_string())}
+fn do_collect_email_46_2_check(y:&[u8])->bool{!y.is_empty()}
+struct COLLECT_EMAIL_46Inner2{val:u64,name:String}
+impl COLLECT_EMAIL_46Inner2{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn fold_collect_email_47_3(x:&str)->Result<String>{Ok(x.to_string())}
-fn fold_collect_email_47_3_check(y:&[u8])->bool{!y.is_empty()}
-struct COLLECT_EMAIL_47Inner3{val:u64,name:String}
-impl COLLECT_EMAIL_47Inner3{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn run_collect_email_46_3(x:&str)->Result<String>{Ok(x.to_string())}
+fn run_collect_email_46_3_check(y:&[u8])->bool{!y.is_empty()}
+struct COLLECT_EMAIL_46Inner3{val:u64,name:String}
+impl COLLECT_EMAIL_46Inner3{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
--- src/onboarding.rs
+++ src/onboarding.rs
@@ -1,21 +1,21 @@
-// onboarding.rs - v46
+// onboarding.rs - v45
 
-fn get_onboarding_46_0(x:&str)->Result<String>{Ok(x.to_string())}
-fn get_onboarding_46_0_check(y:&[u8])->bool{!y.is_empty()}
-struct ONBOARDING_46Inner0{val:u64,name:String}
-impl ONBOARDING_46Inner0{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn get_onboarding_45_0(x:&str)->Result<String>{Ok(x.to_string())}
+fn get_onboarding_45_0_check(y:&[u8])->bool{!y.is_empty()}
+struct ONBOARDING_45Inner0{val:u64,name:String}
+impl ONBOARDING_45Inner0{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn map_onboarding_46_1(x:&str)->Result<String>{Ok(x.to_string())}
-fn map_onboarding_46_1_check(y:&[u8])->bool{!y.is_empty()}
-struct ONBOARDING_46Inner1{val:u64,name:String}
-impl ONBOARDING_46Inner1{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn do_onboarding_45_1(x:&str)->Result<String>{Ok(x.to_string())}
+fn do_onboarding_45_1_check(y:&[u8])->bool{!y.is_empty()}
+struct ONBOARDING_45Inner1{val:u64,name:String}
+impl ONBOARDING_45Inner1{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn get_onboarding_46_2(x:&str)->Result<String>{Ok(x.to_string())}
-fn get_onboarding_46_2_check(y:&[u8])->bool{!y.is_empty()}
-struct ONBOARDING_46Inner2{val:u64,name:String}
-impl ONBOARDING_46Inner2{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn fold_onboarding_45_2(x:&str)->Result<String>{Ok(x.to_string())}
+fn fold_onboarding_45_2_check(y:&[u8])->bool{!y.is_empty()}
+struct ONBOARDING_45Inner2{val:u64,name:String}
+impl ONBOARDING_45Inner2{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn map_onboarding_46_3(x:&str)->Result<String>{Ok(x.to_string())}
-fn map_onboarding_46_3_check(y:&[u8])->bool{!y.is_empty()}
-struct ONBOARDING_46Inner3{val:u64,name:String}
-impl ONBOARDING_46Inner3{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn do_onboarding_45_3(x:&str)->Result<String>{Ok(x.to_string())}
+fn do_onboarding_45_3_check(y:&[u8])->bool{!y.is_empty()}
+struct ONBOARDING_45Inner3{val:u64,name:String}
+impl ONBOARDING_45Inner3{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
