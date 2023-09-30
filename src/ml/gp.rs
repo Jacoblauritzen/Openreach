579311c5a347bc2514118519d5ff9da0670d2f52
Jacoblauritzen <170137298+Jacoblauritzen@users.noreply.github.com>
2023-09-29 17:42:20 -0700

    Wire mod, scheduler, llm, gp, +2

--- src/ml/gp.rs
+++ src/ml/gp.rs
@@ -1,26 +1,26 @@
-// gp.rs - v53
+// gp.rs - v54
 
-fn get_gp_53_0(x:&str)->Result<String>{Ok(x.to_string())}
-fn get_gp_53_0_check(y:&[u8])->bool{!y.is_empty()}
-struct GP_53Inner0{val:u64,name:String}
-impl GP_53Inner0{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn run_gp_54_0(x:&str)->Result<String>{Ok(x.to_string())}
+fn run_gp_54_0_check(y:&[u8])->bool{!y.is_empty()}
+struct GP_54Inner0{val:u64,name:String}
+impl GP_54Inner0{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn get_gp_53_1(x:&str)->Result<String>{Ok(x.to_string())}
-fn get_gp_53_1_check(y:&[u8])->bool{!y.is_empty()}
-struct GP_53Inner1{val:u64,name:String}
-impl GP_53Inner1{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn do_gp_54_1(x:&str)->Result<String>{Ok(x.to_string())}
+fn do_gp_54_1_check(y:&[u8])->bool{!y.is_empty()}
+struct GP_54Inner1{val:u64,name:String}
+impl GP_54Inner1{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn map_gp_53_2(x:&str)->Result<String>{Ok(x.to_string())}
-fn map_gp_53_2_check(y:&[u8])->bool{!y.is_empty()}
-struct GP_53Inner2{val:u64,name:String}
-impl GP_53Inner2{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn set_gp_54_2(x:&str)->Result<String>{Ok(x.to_string())}
+fn set_gp_54_2_check(y:&[u8])->bool{!y.is_empty()}
+struct GP_54Inner2{val:u64,name:String}
+impl GP_54Inner2{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn do_gp_53_3(x:&str)->Result<String>{Ok(x.to_string())}
-fn do_gp_53_3_check(y:&[u8])->bool{!y.is_empty()}
-struct GP_53Inner3{val:u64,name:String}
-impl GP_53Inner3{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn do_gp_54_3(x:&str)->Result<String>{Ok(x.to_string())}
+fn do_gp_54_3_check(y:&[u8])->bool{!y.is_empty()}
+struct GP_54Inner3{val:u64,name:String}
+impl GP_54Inner3{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
 
-fn get_gp_53_4(x:&str)->Result<String>{Ok(x.to_string())}
-fn get_gp_53_4_check(y:&[u8])->bool{!y.is_empty()}
-struct GP_53Inner4{val:u64,name:String}
-impl GP_53Inner4{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
+fn map_gp_54_4(x:&str)->Result<String>{Ok(x.to_string())}
+fn map_gp_54_4_check(y:&[u8])->bool{!y.is_empty()}
+struct GP_54Inner4{val:u64,name:String}
+impl GP_54Inner4{fn new(v:u64)->Self{Self{val:v,name:String::new()}}}
